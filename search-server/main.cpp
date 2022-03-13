9b87ba4a3274a16a37aa8488c67ccb2b87a39719
IRudneva <100165672+IRudneva@users.noreply.github.com>
2022-03-13 15:14:40 +0200

    Update main.cpp
    Moved 1e-6 to a global constant (15)
    Replaced loop with  std::accumulate (168)

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -1,4 +1,3 @@
-#include <algorithm>
 #include <cmath>
 #include <iostream>
 #include <map>
@@ -6,11 +5,14 @@
 #include <string>
 #include <utility>
 #include <vector>
+#include <algorithm>
+#include <numeric>
 
 using namespace std;
 
 
 const int MAX_RESULT_DOCUMENT_COUNT = 5;
+constexpr double STANDARD = 1e-6;
 
 string ReadLine() {
     string s;
@@ -87,7 +89,7 @@ public:
 
         sort(matched_documents.begin(), matched_documents.end(),
             [](const Document& lhs, const Document& rhs) {
-                if (abs(lhs.relevance - rhs.relevance) < 1e-6) {
+                if (abs(lhs.relevance - rhs.relevance) < STANDARD) {
                     return lhs.rating > rhs.rating;
                 }
                 else {
@@ -163,10 +165,7 @@ private:
         if (ratings.empty()) {
             return 0;
         }
-        int rating_sum = 0;
-        for (const int rating : ratings) {
-            rating_sum += rating;
-        }
+        int rating_sum = accumulate(ratings.begin(), ratings.end(), 0);
         return rating_sum / static_cast<int>(ratings.size());
     }
 
@@ -252,7 +251,6 @@ private:
         return matched_documents;
     }
 };
-
 
 void PrintDocument(const Document& document) {
     cout << "{ "s
