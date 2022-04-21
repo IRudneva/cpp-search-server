fe89790d4907fc89997002c7161e0d65d3fc0130
Irina Rudneva <stolya2707@yandex.ru>
2022-04-21 20:28:59 +0200

    Final project. Sprint 4.

---
+++ search-server/read_input_functions.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "search_server.h"
+
+using namespace std;
+
+void PrintDocument(const Document& document);
+
+void PrintMatchDocumentResult(int document_id, const vector<string>& words, DocumentStatus status);
+
+void AddDocument(SearchServer& search_server, int document_id, const string& document, DocumentStatus status, const vector<int>& ratings);
+
+void FindTopDocuments(const SearchServer& search_server, const string& raw_query);
+
+void MatchDocuments(const SearchServer& search_server, const string& query);
